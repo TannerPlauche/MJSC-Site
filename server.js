6d407e24959bc54e1e3659bfd6f9ad3cb2436f3b
Tanner Plauche <tannerplauche@gmail.com>
2018-03-05 22:52:14 -0600

    test auto restart script

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const path = require("path");
 const port = process.env.PORT || 8000;
 const logger = require("morgan");
 
+const runner = require("./shellRunner");
+
 server.use(logger('dev'));
-server.use("/",express.static(path.join(__dirname, "./")));
+server.use("/", express.static(path.join(__dirname, "./")));
 // server.use("/img",express.static(path.join(__dirname, "./img")));
 // server.use("/css",express.static(path.join(__dirname, "./css")));
-server.use("/:any",express.static(path.join(__dirname, "./")));
+server.use("/:any", express.static(path.join(__dirname, "./")));
 
 server.listen(port, () => {
     console.log("running on port", port);
 });
+
+setTimeout(runner, 5000);
---
+++ shellRunner.js
@@ -0,0 +1,20 @@
+const processScript = require("child_process").exec;
+// let script = require("./updateAndRestart.sh");
+// console.log(script);
+function runFile() {
+    console.log("executing");
+    processScript("git pull", function (err) {
+        if (err) { console.warn(err); }
+    });
+
+    processScript("sleep 50000", function (err) {
+        if (err) { console.warn(err); }
+    });
+
+
+    processScript("rs", function (err) {
+        if (err) { console.warn(err); }
+    });
+}
+
+module.exports = runFile;
