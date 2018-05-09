fdd7cecdce75aa302195bde7ca8e8949d024ea08
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2018-05-08 21:46:55 -0500

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 const path = require("path");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8765;
 const logger = require("morgan");
 const runner = require("./shellRunner");
 
