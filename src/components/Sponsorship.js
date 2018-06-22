cb826733ca8dbdb7edf37bb2651dcd1da00218f4
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2018-06-22 18:40:11 -0500

    rebuild

--- src/components/Sponsorship.js
+++ src/components/Sponsorship.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import React,
 {
   Component
 }
 
   from 'react';
-=======
-import React, { Component } from 'react';
->>>>>>> 1d2ef57169659755f3cf354780dcf0f595a060e6
 import '../css/sponsorship.css';
 import sponsorshipInfo from '../assets/graphics/mjscScholarships.png';
 
@@ -16,16 +12,7 @@ export default class Sponsorship extends Component {
     return (
       <div className="section is-marginless sponsorship">
         <h1>Become a sponsor!</h1>
-<<<<<<< HEAD
         <img src={sponsorshipInfo} alt="" className="sponsorshipInfo" />
       </div >);
-=======
-        <img src={sponsorshipInfo}
-          alt=""
-          className="sponsorshipInfo" />
-
-      </div >
-    );
->>>>>>> 1d2ef57169659755f3cf354780dcf0f595a060e6
   }
 }
